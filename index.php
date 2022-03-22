d97ef307163c8c3e7b090b8a414ad92b35a42b63
Sergey Makarov <grinn2102@gmail.com>
2022-03-23 00:03:27 +0300

    Доделал первый пункт ДЗ

--- index.php
+++ index.php
@@ -1,80 +1,101 @@
 <?php
+    // var_dump($_REQUEST);
+    // var_dump($_FILES);
+    // var_dump($_SERVER);
+    $uploadDir = './upload/';
+    $existsDir = file_exists($uploadDir);
 
-$num = 0;
-$result = null;
-
-while($num <= 100){
-    $res = $num % 3;
-    if($res == 0){
-        $result .= ' '.$num;
-    }
-
-    $num++;
-}
-
-$res = getNumbers(0);
-
-function getNumbers($number){
-    $zeroString = 'ноль';
-    $oddString = 'нечетное число';
-    $evenString = 'четное число';
-    $res = null;
-    do {
-        if($number == 0){
-            $res .= $number . " - $zeroString </br>";
-        } else {
-            $res .= $number % 2 ? $number . " - $oddString </br>" : $number . " - $evenString </br>";
+    if(@$_REQUEST['doPush'])
+    {
+        if(!$existsDir)
+        {
+            mkdir($uploadDir, 0777);
         }
-        $number++;
-    } while ($number <= 10);
-    return $res;
-}
 
-$arCity = [
-    'Московская область' => ['Москва', 'Зеленоград', 'Клин'],
-    'Ленинградская область' => ['Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт'],
-    'Рязанская область' => ['Рязань', 'Касимов', 'Сасово', 'Михайлов'],
-    'Костромская область' => ['Кострома', 'Сусанино', 'Буй', 'Чухлома']
-];
+        $tmpFile = $_FILES['files']['tmp_name'];
+        $nameFile = $_FILES['files']['name'];
+        $checkFiles = is_uploaded_file($tmpFile);
 
-$city = getCity($arCity);
-
-function getCity($array){
-    $res = null;
-    $region = array_keys($array);
-    for($i = 0; $i < count($array); $i++ ){
-        $res .= $region[$i] . ': <br>' . implode($array[$region[$i]], ', '). '.'. '<br>';
+        if($checkFiles)
+        {
+            move_uploaded_file($tmpFile, $uploadDir.$nameFile);
+            // echo "Файл $nameFile успешно загружен";
+        }
     }
-    return $res;
-}
 
-$arMenu = ['Главная', 'Контент', 'Новости', 'Клиентам'];
+    $arPhotos = getArPhotos($uploadDir);
 
-$menuList = getMenu($arMenu);
-
-function getMenu($array){
-    $res = null;
-    for($i = 0; $i < count($array); $i++ ){
-        $res .= '<ul>' . '<li>' . $array[$i] . '</li>' . '</ul>';
+    function getArPhotos($path)
+    {
+        $arPhotos = [];
+        $arFiles = [];
+        $arFiles = glob("$path*");
+        foreach($arFiles as $path)
+        {
+            $sz = getimagesize($path);
+            $tm = filemtime($path);
+            $arPhotos[$tm] = [
+                'time' => $tm,
+                'name' => basename($path),
+                'url' => $path,
+                'w'=> $sz[0],
+                'h'=> $sz[1],
+                'wh'=> $sz[3],
+            ];
+        }
+        return $arPhotos;
     }
-    return $res;
-}
 
-$arData = ['result' => $result, 'res' => $res, 'city' => $city, 'menuList' => $menuList];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Галерея</title>
+    <style>
+        .container {
+            width: 1170px;
+            margin: 0 auto;
+        }
+        .gallery {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            margin: 50px 0;
+        }
+        .form {
+            display: flex;
+            justify-content: center;
+        }
 
-$menu = renderTemplate('menu', $arData);
-$content = renderTemplate('content', $arData);
-
-$arTemplate = ['menu' => $menu , 'content' => $content];
-
-$template = renderTemplate('template', $arTemplate);
-
-function renderTemplate($template = '', $arParams = [])
-{
-    extract($arParams);
-    ob_start();
-    include $template.'.php';
-    return ob_get_clean();
-}
-
-echo $template;
+        .picture {
+            padding: 20px;
+            border: solid 1px #000;
+            margin: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <?if($arPhotos){?>
+            <div class="gallery">
+                <?foreach($arPhotos as $key => $photo){?>
+                    <a href="<?=$photo['url']?>" target="_blank">
+                        <img class="picture" src="<?=$photo['url']?>" alt="<?=$photo['name']?>" width="100" height="100">
+                    </a>
+                <?}?>
+            </div>
+        <?}else{?>
+            <div class="gallery">
+                <p>Галерея пуста, загрузите фото</p>
+            </div>
+        <?}?>    
+        <form class="form" action="<?=$_SERVER['SCRIPT_NAME']?>" method="POST" enctype="multipart/form-data">
+            <input type="file" name="files">
+            <input type="submit" value="Загрузить" name="doPush">
+        </form>
+    </div>
+</body>
+</html>
